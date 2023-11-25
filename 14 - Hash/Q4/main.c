ae35e6e5bc8c02b2ffeaf6f0a256d45c786738d8
Enzo Moreira Paião <82907775+Enzo-paiao@users.noreply.github.com>
2023-11-25 12:44:25 -0300

    Update main.c

--- 14 - Hash/Q4/main.c
+++ 14 - Hash/Q4/main.c
@@ -4,6 +4,7 @@
 
 #define TABLE_SIZE 10
 
+// Definindo a estrutura do funcionário
 typedef struct Employee {
   char name[50];
   char hireDate[11];
@@ -11,63 +12,62 @@ typedef struct Employee {
   struct Employee *next;
 } Employee;
 
-typedef struct HashTable {
-  Employee *table[TABLE_SIZE];
-} HashTable;
-
-int hashFunction(char *name) {
-  int hash = 0;
-  while (*name) {
-    hash += *name++;
+// Função para criar um novo funcionário
+Employee *createEmployee(char *name, char *hireDate, float salary) {
+  Employee *newEmployee = (Employee *)malloc(sizeof(Employee));
+  if (newEmployee != NULL) {
+    strcpy(newEmployee->name, name);
+    strcpy(newEmployee->hireDate, hireDate);
+    newEmployee->salary = salary;
+    newEmployee->next = NULL;
   }
-  return hash % TABLE_SIZE;
+  return newEmployee;
 }
 
-void insertEmployee(HashTable *hashTable, char *name, char *hireDate,
+// Função para inserir um funcionário na tabela hash
+void insertEmployee(Employee **hashTable, char *name, char *hireDate,
                     float salary) {
   int index = hashFunction(name);
+  Employee *newEmployee = createEmployee(name, hireDate, salary);
 
-  Employee *newEmployee = (Employee *)malloc(sizeof(Employee));
-  strcpy(newEmployee->name, name);
-  strcpy(newEmployee->hireDate, hireDate);
-  newEmployee->salary = salary;
-  newEmployee->next = NULL;
-
-  Employee *current = hashTable->table[index];
-  Employee *previous = NULL;
-
-  while (current != NULL && strcmp(name, current->name) > 0) {
-    previous = current;
-    current = current->next;
-  }
-
-  if (previous == NULL) {
-    newEmployee->next = hashTable->table[index];
-    hashTable->table[index] = newEmployee;
+  if (hashTable[index] == NULL) {
+    hashTable[index] = newEmployee;
   } else {
-    newEmployee->next = current;
-    previous->next = newEmployee;
+    // Inserção ordenada na lista encadeada
+    Employee *current = hashTable[index];
+    Employee *prev = NULL;
+
+    while (current != NULL && strcmp(name, current->name) > 0) {
+      prev = current;
+      current = current->next;
+    }
+
+    if (prev == NULL) {
+      newEmployee->next = hashTable[index];
+      hashTable[index] = newEmployee;
+    } else {
+      prev->next = newEmployee;
+      newEmployee->next = current;
+    }
   }
 }
 
-Employee *searchByName(HashTable *hashTable, char *name) {
+// Função para pesquisar funcionário por nome
+Employee *searchByName(Employee **hashTable, char *name) {
   int index = hashFunction(name);
-  Employee *current = hashTable->table[index];
+  Employee *current = hashTable[index];
 
-  while (current != NULL) {
-    if (strcmp(name, current->name) == 0) {
-      return current;
-    }
+  while (current != NULL && strcmp(name, current->name) != 0) {
     current = current->next;
   }
 
-  return NULL;
+  return current;
 }
 
-void searchByDate(HashTable *hashTable, char *hireDate) {
+// Função para pesquisar funcionários por data de admissão
+void searchByDate(Employee **hashTable, char *hireDate) {
   for (int i = 0; i < TABLE_SIZE; i++) {
-    Employee *current = hashTable->table[i];
-
+    Employee *current = hashTable[i];
     while (current != NULL) {
       if (strcmp(hireDate, current->hireDate) == 0) {
         printf("Nome: %s, Data de Admissao: %s, Salario: %.2f\n", current->name,
@@ -78,150 +78,169 @@ void searchByDate(HashTable *hashTable, char *hireDate) {
   }
 }
 
-void deleteEmployee(HashTable *hashTable, char *name) {
+// Função para excluir funcionário
+void deleteEmployee(Employee **hashTable, char *name) {
   int index = hashFunction(name);
-  Employee *current = hashTable->table[index];
-  Employee *previous = NULL;
+  Employee *current = hashTable[index];
+  Employee *prev = NULL;
 
   while (current != NULL && strcmp(name, current->name) != 0) {
-    previous = current;
+    prev = current;
     current = current->next;
   }
 
-  if (current == NULL) {
-    printf("Funcionario nao encontrado.\n");
-    return;
-  }
+  if (current != NULL) {
+    if (prev == NULL) {
+      hashTable[index] = current->next;
+    } else {
+      prev->next = current->next;
+    }
 
-  if (previous == NULL) {
-    hashTable->table[index] = current->next;
+    free(current);
   } else {
-    previous->next = current->next;
+    printf("Funcionario nao encontrado.\n");
   }
-
-  free(current);
-  printf("Funcionario excluido com sucesso.\n");
 }
 
-void displayHashTable(HashTable *hashTable) {
+// Função para imprimir a tabela hash
+void printHashTable(Employee **hashTable) {
   for (int i = 0; i < TABLE_SIZE; i++) {
-    printf("Entrada %d: ", i);
-    Employee *current = hashTable->table[i];
+    Employee *current = hashTable[i];
     while (current != NULL) {
-      printf("Nome: %s, Data de Admissao: %s, Salario: %.2f -> ", current->name,
+      printf("Nome: %s, Data de Admissao: %s, Salario: %.2f\n", current->name,
              current->hireDate, current->salary);
       current = current->next;
     }
-    printf("NULL\n");
   }
 }
 
-void saveToFile(HashTable *hashTable) {
+// Função para calcular o índice na tabela hash
+int hashFunction(char *name) {
+  int hash = 0;
+  for (int i = 0; name[i] != '\0'; i++) {
+    hash += name[i];
+  }
+  return hash % TABLE_SIZE;
+}
+
+// Função para salvar a tabela hash em um arquivo binário
+void saveToFile(Employee **hashTable) {
   FILE *file = fopen("employee_data.bin", "wb");
+  if (file != NULL) {
+    fwrite(hashTable, sizeof(Employee *), TABLE_SIZE, file);
 
-  if (file == NULL) {
-    perror("Erro ao abrir o arquivo");
-    exit(EXIT_FAILURE);
-  }
-
-  for (int i = 0; i < TABLE_SIZE; i++) {
-    Employee *current = hashTable->table[i];
-
-    while (current != NULL) {
-      fwrite(current, sizeof(Employee), 1, file);
-      current = current->next;
+    for (int i = 0; i < TABLE_SIZE; i++) {
+      Employee *current = hashTable[i];
+      while (current != NULL) {
+        fwrite(current, sizeof(Employee), 1, file);
+        current = current->next;
+      }
     }
-  }
 
-  fclose(file);
-  printf("Tabela hash salva no arquivo com sucesso.\n");
+    fclose(file);
+  }
 }
 
-void loadFromFile(HashTable *hashTable) {
+// Função para carregar a tabela hash de um arquivo binário
+void loadFromFile(Employee **hashTable) {
   FILE *file = fopen("employee_data.bin", "rb");
+  if (file != NULL) {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+      hashTable[i] = NULL; // Inicializa cada entrada da tabela como NULL
+      Employee *prev = NULL;
 
-  if (file == NULL) {
-    perror("Erro ao abrir o arquivo");
-    return;
+      while (1) {
+        Employee *current = (Employee *)malloc(sizeof(Employee));
+        if (fread(current, sizeof(Employee), 1, file) != 1) {
+          free(current);
+          break;
+        }
+
+        current->next = NULL;
+
+        if (prev == NULL) {
+          hashTable[i] = current;
+        } else {
+          prev->next = current;
+        }
+
+        prev = current;
+      }
+    }
+
+    fclose(file);
   }
-
-  Employee temp;
-  while (fread(&temp, sizeof(Employee), 1, file) == 1) {
-    insertEmployee(hashTable, temp.name, temp.hireDate, temp.salary);
-  }
-
-  fclose(file);
-  printf("Tabela hash carregada do arquivo com sucesso.\n");
 }
 
+// Função principal
 int main() {
-  HashTable hashTable;
-  memset(&hashTable, 0, sizeof(HashTable));
+  Employee *hashTable[TABLE_SIZE] = {NULL};
 
-  loadFromFile(&hashTable);
+  // Carrega dados do arquivo se existir
+  loadFromFile(hashTable);
 
   int choice;
-  char name[50];
-  char hireDate[11];
+  char name[50], hireDate[11];
   float salary;
 
   do {
-    printf("\n1. Cadastrar funcionario\n");
-    printf("2. Pesquisar por nome\n");
-    printf("3. Pesquisar por data de admissao\n");
-    printf("4. Excluir funcionario\n");
-    printf("5. Exibir tabela hash\n");
-    printf("6. Salvar tabela hash em arquivo\n");
-    printf("7. Sair\n");
-
+    printf("\n1. Cadastrar Funcionario\n");
+    printf("2. Pesquisar Funcionario por Nome\n");
+    printf("3. Pesquisar Funcionario por Data de Admissao\n");
+    printf("4. Excluir Funcionario\n");
+    printf("5. Mostrar Tabela Hash\n");
+    printf("6. Salvar e Sair\n");
     printf("Escolha uma opcao: ");
     scanf("%d", &choice);
 
     switch (choice) {
     case 1:
-      printf("Digite o nome do funcionario: ");
+      printf("Nome: ");
       scanf("%s", name);
-      printf("Digite a data de admissao (dd/mm/aaaa): ");
+      printf("Data de Admissao (dd/mm/aaaa): ");
       scanf("%s", hireDate);
-      printf("Digite o salario: ");
+      printf("Salario: ");
       scanf("%f", &salary);
-      insertEmployee(&hashTable, name, hireDate, salary);
+      insertEmployee(hashTable, name, hireDate, salary);
       break;
+
     case 2:
-      printf("Digite o nome a ser pesquisado: ");
+      printf("Nome: ");
       scanf("%s", name);
-      Employee *result = searchByName(&hashTable, name);
+      Employee *result = searchByName(hashTable, name);
       if (result != NULL) {
-        printf("Nome: %s, Data de Admissao: %s, Salario: %.2f\n", result->name,
-               result->hireDate, result->salary);
+        printf("Funcionario encontrado - Nome: %s, Data de Admissao: %s, "
+               "Salario: %.2f\n",
+               result->name, result->hireDate, result->salary);
       } else {
         printf("Funcionario nao encontrado.\n");
       }
       break;
+
     case 3:
-      printf("Digite a data de admissao a ser pesquisada (dd/mm/aaaa): ");
+      printf("Data de Admissao (dd/mm/aaaa): ");
       scanf("%s", hireDate);
-      searchByDate(&hashTable, hireDate);
+      searchByDate(hashTable, hireDate);
       break;
+
     case 4:
-      printf("Digite o nome do funcionario a ser excluido: ");
+      printf("Nome: ");
       scanf("%s", name);
-      deleteEmployee(&hashTable, name);
+      deleteEmployee(hashTable, name);
       break;
+
     case 5:
-      displayHashTable(&hashTable);
+      printHashTable(hashTable);
       break;
+
     case 6:
-      saveToFile(&hashTable);
+      saveToFile(hashTable);
       break;
-    case 7:
-      saveToFile(&hashTable);
-      printf("Programa encerrado.\n");
-      break;
+
     default:
-      printf("Opcao invalida.\n");
+      printf("Opcao invalida. Tente novamente.\n");
     }
-  } while (choice != 7);
+  } while (choice != 6);
 
   return 0;
 }
